038878290770f0241d746bcde7652158a1cd1134
Igor Fernandes <palusterra@gmail.com>
2023-05-20 21:04:51 -0300

    finalizando projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,3 +103,15 @@ const scrollActive = () => {
 window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+})
+
+sr.reveal(`.home__data, .home__social, contact__container, .footer__container`)
+sr.reveal(`.home__image`, {origin: 'bottom'})
+sr.reveal(`.about__data, .skills__data`,  {origin: 'left'})
+sr.reveal(`.about__image, .skills__content`, {origin: 'right'})
+sr.reveal(`.services__card, .projects__card`, {interval: 100})
